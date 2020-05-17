d221df74cc0e25f8c0c63c5bb89c230d13388229
Bahauddin Aziz <bahauddinaziz@gmail.com>
2020-05-17 18:40:12 +0530

    basic UI

--- src/index.js
+++ src/index.js
@@ -1,51 +1,7 @@
+
+
 //enabling ethereum
 window.ethereum.enable()
-
-// varibles-DOM
-var masterKey;
-var account = {
-    "accountName" : "",
-    "Username" : "",
-    "Password" : ""
-}
-var search;
-
-
-
-// functions-DOM
-
-function getMasterKey(event){
-    masterKey = event.target.value;
-}
-
-async function setMasterKey(){
-   await trusuesContract.methods.setMasterKey(masterKey).send({from:address});
-   console.log("masterKey Added");
-}
-
-function getAccount(event){
-    let name = event.target.name;
-    let val = event.target.value;
-    
-    account[name] = val; 
-    
-}
-
-
-async function addAccount(){
-    console.log(account)
-    await trusuesContract.methods.addAccount(account.accountName,account.Username,account.Password).send({from : address});
-    console.log("done")
-}
-
-function setQuery(event){
-    search = event.target.value;
-}
-
-function getResult(){
-    console.log(address,masterKey, search)
-    trusuesContract.methods.mySearch(address,masterKey,search).call().then(res => {console.log(res)})
-}
 
 // varibles - contract
 var web3 = new Web3( Web3.givenProvider || 'HTTP://127.0.0.1:7545');
@@ -129,4 +85,59 @@ var abi =   [
     }
   ]
 var trusuesContract = new web3.eth.Contract(abi,"0x411fD19601Cb5b98B9321Ccf436FD63926A528E3");
-console.log(web3.currentProvider.selectedAddress)
+console.log(address)
+
+
+// varibles-DOM
+var privateKey;
+var masterKey;
+var account = {
+    "accountName" : "",
+    "Username" : "",
+    "Password" : ""
+}
+var search;
+
+
+
+// functions-DOM
+
+privateKey = 'ECAC051BFCE03D4C19D25355200738BE3B4F905C302B5351BEE3763CF36F30DA'
+
+function getMasterKey(event){
+    masterKey = event.target.value;
+}
+
+async function setMasterKey(){
+  console.log(masterKey)
+   trusuesContract.methods.setMasterKey(masterKey).send({from:address}).then(() => { console.log("masterKey Added"); });
+}
+
+function getAccount(event){
+    let name = event.target.name;
+    let val = event.target.value;
+    
+    account[name] = val; 
+
+}
+
+
+async function addAccount(){
+    console.log(account)
+    await trusuesContract.methods.addAccount(account.accountName,account.Username,account.Password).send({from : address});
+    console.log("done")
+}
+
+function setQuery(event){
+    search = event.target.value;
+}
+
+function getResult(){
+    trusuesContract.methods.mySearch(address,masterKey,search).call().then(res => {
+      let user = res.Username;
+      let pass = res.Password;
+      document.getElementById('accounts').innerHTML = user + '<br>' + pass;
+    })
+}
+
+
